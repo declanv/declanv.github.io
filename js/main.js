2cb8fc47c1bed20c2be228ea78caa1c259471a43
Crustacean87! <declanv@gmail.com>
2014-08-04 23:00:22 -0400

    Adding in JS functionality for animating name

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
 $(function() {
 
-  // $(".name").load("name_svg.html", function(){
-  //   console.log("name loaded");
-  // });
+  $(".name").load("name_svg.html", function(){
+    var namePaths = document.querySelectorAll('.name-path');
+
+    $(namePaths).each(function(nPath){
+
+      var length = paths[nPath].getTotalLength();
+
+      paths[nPath].style.transition = paths[nPath].style.WebkitTransition =
+      'none';
+
+      paths[nPath].style.strokeDasharray = length + ' ' + length;
+      paths[nPath].style.strokeDashoffset = length;
+
+      paths[nPath].getBoundingClientRect();
+
+      paths[nPath].style.transition = paths[nPath].style.WebkitTransition =
+      'stroke-dashoffset 5s ease-in-out';
+
+      paths[nPath].style.strokeDashoffset = '0';
+
+    });
+
+  });
+
+  });
+
+
 
   $( ".hi-intro-wrap" ).load( "desktop_svg.html", function() {
 
