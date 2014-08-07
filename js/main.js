db2024340758e5838f298a562d8adacd71162bd6
Crustacean87! <declanv@gmail.com>
2014-08-07 09:21:21 -0400

    Attempting to fix cross-browser compatibility by removing the JQuery load

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function() {
 
-  $(".name-wrap").load("name_svg.html");//, function(){
+  // $(".name-wrap").load("name_svg.html");//, function(){
     // var namePaths = document.querySelectorAll('.name-path');
     // console.log("name loaded, and here are the namepaths", namePaths);
     // $(namePaths).each(function(nPath){
@@ -24,29 +24,29 @@ $(function() {
 
   // });
 
-  $( ".hi-intro-wrap" ).load( "desktop_svg.html", function() {
+  // $( ".hi-intro-wrap" ).load( "desktop_svg.html", function() {
 
-    var paths = document.querySelectorAll('.squiggle-animated');
+  //   var paths = document.querySelectorAll('.desktop_path');
 
-    $(paths).each(function(path){
+  //   $(paths).each(function(path){
 
-      var length = paths[path].getTotalLength();
+  //     var length = paths[path].getTotalLength();
 
-      paths[path].style.transition = paths[path].style.WebkitTransition =
-      'none';
+  //     paths[path].style.transition = paths[path].style.WebkitTransition =
+  //     'none';
 
-      paths[path].style.strokeDasharray = length + ' ' + length;
-      paths[path].style.strokeDashoffset = length;
+  //     paths[path].style.strokeDasharray = length + ' ' + length;
+  //     paths[path].style.strokeDashoffset = length;
 
-      paths[path].getBoundingClientRect();
+  //     paths[path].getBoundingClientRect();
 
-      paths[path].style.transition = paths[path].style.WebkitTransition =
-      'stroke-dashoffset 5s ease-in-out';
+  //     paths[path].style.transition = paths[path].style.WebkitTransition =
+  //     'stroke-dashoffset 5s ease-in-out';
 
-      paths[path].style.strokeDashoffset = '0';
+  //     paths[path].style.strokeDashoffset = '0';
 
-    });
+  //   });
 
-  });
+  // });
 
 });
