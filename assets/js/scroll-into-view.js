c38a4b6bd011553212695ee697d10ac907ef9d88
Declan Van Welie <declanv@gmail.com>
2018-02-13 21:43:38 -0500

    Adding: scroll-into-view js and css animations

---
+++ assets/js/scroll-into-view.js
@@ -0,0 +1,43 @@
+//http://tobiasahlin.com/moving-letters/# --> Out Now animation		
+$(document).ready(function(){
+
+	// http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+	function isElementInViewport (el) 
+	{
+	    //special bonus for those using jQuery
+	    if (typeof jQuery !== 'undefined' && el instanceof jQuery) el = el[0];
+
+	    var rect = el.getBoundingClientRect();
+	    var windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+	    var windowWidth = (window.innerWidth || document.documentElement.clientWidth);
+
+	    return (
+	           (rect.left >= 0)
+	        && (rect.top >= 0)
+	        && ((rect.left + rect.width) <= windowWidth)
+	        && ((rect.top + rect.height) <= windowHeight)
+	    );
+
+	}
+
+	var scroll = window.requestAnimationFrame ||
+            function(callback){window.setTimeout(callback, 1000/60)};
+    var elementsToShow = document.querySelectorAll('.show-on-scroll');
+
+    function loop() {
+
+	  elementsToShow.forEach(function (element) {
+	    if (isElementInViewport(element)) {
+	    	console.log('visible')
+	      element.classList.add('visible');
+	    } else {
+	    	console.log('invisible')
+	      element.classList.remove('visible');
+	    }
+	  });
+
+	  scroll(loop);
+	}
+
+	loop();
+});
