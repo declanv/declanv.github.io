9a566bf977e2cccdbfff95103ac7880c540015af
Declan Van Welie <declanv@gmail.com>
2018-02-13 21:55:01 -0500

    Attempting to add a means to turn off the more complex animation after the first view

--- assets/js/scroll-into-view.js
+++ assets/js/scroll-into-view.js
@@ -23,16 +23,26 @@ $(document).ready(function(){
 	var scroll = window.requestAnimationFrame ||
             function(callback){window.setTimeout(callback, 1000/60)};
     var elementsToShow = document.querySelectorAll('.show-on-scroll');
+	seenYet = false;
 
     function loop() {
 
 	  elementsToShow.forEach(function (element) {
+	  	console.log('seenYet: ', seenYet);
 	    if (isElementInViewport(element)) {
 	    	console.log('visible')
 	      element.classList.add('visible');
+	      if (seenYet == false) {
+	      	element.classList.add('play');
+	      	// seenYet = true;
+	      } else {
+	      	element.classList.remove('play');
+	      }
 	    } else {
 	    	console.log('invisible')
 	      element.classList.remove('visible');
+     	  element.classList.remove('play');
+     	  // seenYet = true;
 	    }
 	  });
 
