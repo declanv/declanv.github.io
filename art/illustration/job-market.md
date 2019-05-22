e440d73d82348f5cd38af42ac911042f57d005e3
Declan Van Welie <declanv@gmail.com>
2019-05-22 07:02:37 -0400

    Adding: images loaded js plugin back in, which seems to fix the masonry issue for the illustration page. Also added in the job market thumb and larger image

--- art/illustration/job-market.md
+++ art/illustration/job-market.md
@@ -5,7 +5,7 @@ type: art, illustration
 description: Personal Illustration
 alt: illustration of dogs watching ball
 medium: Watercolor, Graphite, and Digital 
-large-image: job-market-large.jpeg
-small-image: job-market-large.jpeg
+large-image: job-market-large.jpg
+small-image: job-market-small.jpg
 size: 752x1000
 ---
--- assets/js/page-transitions.js
+++ assets/js/page-transitions.js
@@ -433,14 +433,43 @@ $( document ).ready(function() {
     }
   };
 
-  $('.grid').masonry({
-    // itemSelector: '.grid-item',
-    // columnWidth: '.grid-sizer',
-    // columnWidth: 300,
-    percentPosition: true,
-    // fitWidth: true,
-    gutter: 30
-  });
+  // $('.grid').masonry({
+  //   // itemSelector: '.grid-item',
+  //   // columnWidth: '.grid-sizer',
+  //   // columnWidth: 300,
+  //   percentPosition: true,
+  //   // fitWidth: true,
+  //   gutter: 30
+  // });
+
+
+    $('.grid').imagesLoaded().done( function( instance ) {
+        console.log('all images successfully loaded');
+
+        initPhotoSwipeFromDOM('.projects-container');
+
+
+        $('.grid').masonry({
+            // itemSelector: '.grid-item',
+            // columnWidth: '.grid-sizer',
+            // columnWidth: 300,
+            percentPosition: true,
+            // fitWidth: true,
+            gutter: 30
+        });
+
+    })
+    .always( function( instance ) {
+        console.log('all images loaded');
+    })
+
+    .fail( function() {
+        console.log('all images loaded, at least one is broken');
+    })
+    .progress( function( instance, image ) {
+        var result = image.isLoaded ? 'loaded' : 'broken';
+        console.log( 'image is ' + result + ' for ' + image.img.src );
+    });
 
   // var transitionContainer = $("#transition-shape");
   //
