ccb4ead4300d0bb1b3dfd554723ee90e43a2805d
Declan Van Welie <declanv@gmail.com>
2019-06-18 22:30:01 -0400

    Adding: unblur transition js and css animation

--- assets/js/gallery-custom.js
+++ assets/js/gallery-custom.js
@@ -1,7 +1,36 @@
 $(function() {
+    var lazyImages = [].slice.call(document.querySelectorAll('img.lazy'));
+    console.log("here are the lazyImages: ", lazyImages);
+    if ('IntersectionObserver' in window) {
+        console.log('the intersection observer check works');
+        config = {
+            // If the image gets within 250px of the browser's viewport,
+            // start the download:
+            rootMargin: '250px 0px',
+        };
+        var lazyImageObserver = new IntersectionObserver(
+            function(entries, observer) {
+                entries.forEach(function(entry) {
+                    if (entry.isIntersecting) {
+                        var lazyImage = entry.target;
+                        lazyImage.src = lazyImage.dataset.src;
+                        lazyImage.classList.add('unblur');
+                        lazyImageObserver.unobserve(lazyImage);
+                    }
+                });
+            }, config);
+        lazyImages.forEach(function(lazyImage) {
+            lazyImageObserver.observe(lazyImage);
+        });
+    } else {
+        // For browsers that don't support IntersectionObserver yet,
+        // load all the images now:
+        lazyImages.forEach(function(lazyImage) {
+            lazyImage.src = lazyImage.dataset.src;
+        });
+    }
 
-
-	// $('.grid').masonry({
+    // $('.grid').masonry({
 	// 	columnWidth: 200,
 	// 	itemSelector: '.grid-item'
 	// });
@@ -261,3 +290,5 @@ initPhotoSwipeFromDOM('.projects-container');
 
 
 });
+
+
